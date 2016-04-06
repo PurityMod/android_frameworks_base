82a16574de69e945f5f4f65d5ed1ff9fc0031d88
AndroidRul3z <ferrettidario@gmail.com>
2016-04-06 18:27:59 +0200

    Purity is starting/upgrading based on https://github.com/PureNexusProject/android_frameworks_base/commit/181c7226a08bdabe775da939a4294f41730b5eff

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -7022,9 +7022,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         }
                     };
                     if (mContext.getPackageManager().isUpgrade()) {
-                        mBootMsgDialog.setTitle(R.string.android_upgrading_title);
+                        mBootMsgDialog.setTitle(R.string.android_upgrading_title_pn);
                     } else {
-                        mBootMsgDialog.setTitle(R.string.android_start_title);
+                        mBootMsgDialog.setTitle(R.string.android_start_title_pn);
                     }
                     mBootMsgDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
                     mBootMsgDialog.setIcon(com.android.internal.R.drawable.boot_logo);
